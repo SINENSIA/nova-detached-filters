560263530d6e8c2c0e497b2518e9905456edfc7a
Carlos Arranz <carlos_arranz@yahoo.com>
2026-01-18 18:22:21 +0000

    fix: solved missing mixings for syncFilters

--- resources/js/mixins/index.js
+++ resources/js/mixins/index.js
@@ -1,1 +1,4 @@
 export { default as RouteParameters } from './RouteParameters';
+export { default as Filterable } from './Filterable';
+export { default as PerPageable } from './PerPageable';
+export { default as InteractsWithQueryString } from './InteractsWithQueryString';
